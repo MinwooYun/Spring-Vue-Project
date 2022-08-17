61bea81af98db40ede4e8d1ba69808cf3d677e34
minwoo <yunms3@gmail.com>
2022-08-18 00:44:53 +0900

    first commit

---
+++ src/main/java/com/vue/minwoo/MinwooApplication.java
@@ -0,0 +1,13 @@
+package com.vue.minwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinwooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinwooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vue/minwoo/MinwooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vue.minwoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinwooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
